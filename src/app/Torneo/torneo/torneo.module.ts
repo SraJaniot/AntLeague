8621dd7538583790e36172eb911d211fb7a28e24
Vanessa <wafer0210@outlook.com>
2023-08-27 17:41:31 -0500

    acá estoy creando para lo que quiero mostrar en cada espacio

---
+++ src/app/Torneo/torneo/torneo.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TorneoRoutingModule } from './torneo-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TorneoRoutingModule
+  ]
+})
+export class TorneoModule { }
